87f12859bf8f631b9c2c272e399d286fb4359f26
FonnDuh <itaic1901@gmail.com>
2025-07-09 20:49:17 +0300

    Added cloud frontend url

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,11 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-  origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+  origin: [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "https://card-manager-wkqg.onrender.com",
+  ],
 });
 
 module.exports = corsMiddleware;
